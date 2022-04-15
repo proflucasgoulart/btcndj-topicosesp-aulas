dd5479290da7aae109c412cbeff73a621bad1e99
proflucasgoulart <103764493+proflucasgoulart@users.noreply.github.com>
2022-04-15 13:28:21 -0300

    Delete src directory

--- src/calcula-valor.js
+++
@@ -1,8 +0,0 @@
-function calcularMontante(capital, taxa, periodo) {
-  let montante = capital * Math.pow((1 + taxa), periodo - 1);
-  return montante;
-}
-
-module.exports = {
-  calcularMontante
-}
