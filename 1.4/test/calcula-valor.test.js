bd5e4862c347c7bc892a4ebbcdf9d53638020a01
proflucasgoulart <103764493+proflucasgoulart@users.noreply.github.com>
2022-04-15 16:12:12 -0300

    Update calcula-valor.test.js

--- 1.4/test/calcula-valor.test.js
+++ 1.4/test/calcula-valor.test.js
@@ -20,7 +20,18 @@ describe('calcularMontante', () => {
 
 describe('arredondar', () => {
   test('Arredondar em duas casas decimais', () => {
+    
     const resultado = calculaValor.arredondar(538.4453124999998);
     expect(resultado).toBe(538.45);
+    
+    // TESTE 01 => expect(538.456).toBeCloseTo(538.46);
+    // Apesar da diferença entre os valores do TESTE 01,
+    // ele irá passar devido ao usoa da função toBeCloseTo.
+    
+    // TESTE 02 => expect(538.456).toBeCloseTo(538.46, 3);
+    // Porém, no teste 02, o segundo parâmetro passado para a função
+    // toBeCloseTo define uma precisão de 3 casas decimais. Nesse caso,
+    // o teste comparando os mesmos valores irá falhar.
+    
   });
 });
